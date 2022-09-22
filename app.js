aefd7cddba61afdb45b944b915d5ee2d677977ca
uagrmsistema1 <ronald.ardaya@gmail.com>
2022-09-22 15:03:39 -0400

    listos para subir a produccion4

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express().use(bodyParser.json());
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 
 app.post('/webhook', (req, res) =>{
     console.log('POST: webhook');
@@ -14,6 +18,16 @@ app.post('/webhook', (req, res) =>{
             //se reciben y procesan los mensajes
             const webhookEvent = entry.messaging[0];
             console.log(webhookEvent);
+
+            const sender_psid = webhookEvent.sender.id;
+            console.log(`Sender PSID; ${sender_psid}`);
+
+            //validar que estamos recibiendo un mensaje
+            if(webhookEvent.message){
+                handleMessage(sender_psid, webhookEvent.message);
+            }else if(webhookEvent.postback){
+                handlePostback(sender_psid, webhookEvent.postback);
+            }
         });
 
         res.status(200).send('EVENTO RECIBIDO');
@@ -46,6 +60,39 @@ app.get('/webhook', (req, res) =>{
 app.get('/', (req, res) => {
     res.status(200).send('Hola a mi bot')
 });
+
+function handleMessage(sender_psid, received_message) {
+    let response;
+    if (received_message.text) {
+        response = {
+            'text': `Tu mensaje fue: ${received_message.text} :)` 
+        };
+    }
+    callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+    const requestBody = { 
+        'recipient': {
+            'id': sender_psid
+        },
+        'message': response
+    };
+
+    request({
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': {'access_token': PAGE_ACCESS_TOKEN},
+        'method': 'POST',
+        'json': requestBody
+
+    }, (err, res, body) =>{
+        if (!err) {
+            console.log('Mensaje enviado de vuelta');
+        }else{
+            console.error('Imposible enviar mensaje :(')
+        }
+    });
+}
 
 const PORT = process.env.PORT || 3000
 
